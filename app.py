1f2946720eb397f3a2040e32d2a10d20f1e1c46c
gurusreedhar-18 <gurudhanush832gmail.com>
2026-04-28 17:14:26 +0530

    test route fix

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return "Working on Render 🚀"
 
 
 @app.route("/test-url", methods=["GET", "POST"])
